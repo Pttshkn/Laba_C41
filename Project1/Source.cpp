09ec85a3f2eab36417e47e69c2e4bccbf127514d
Rox1337 <vmv8@tpu.ru>
2024-12-07 11:43:15 +0700

    add comm and change bitset

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <iomanip>
 #include <bitset>
-
+//add comm for teammate
+// switch bitset in 8 to 32
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    bitset<8> n = -1;
+    bitset<32> n = -1;
     for (int i = 0; i <= 4; ++i) {
         cout << (n >> i * 2 << i) << endl; //2 раза вправо и 1 раз влево
     }
